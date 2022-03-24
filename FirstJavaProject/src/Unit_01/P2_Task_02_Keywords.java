b34f7177df4124d26875bce42f102ab402f839ce
acer <acer@LAPTOP-9IORETPM>
2022-03-24 16:16:36 +0530

    Keyword as variable,function,object error

---
+++ FirstJavaProject/src/Unit_01/P2_Task_02_Keywords.java
@@ -0,0 +1,27 @@
+package Unit_01;
+
+public class P2_Task_02_Keywords {
+	class Student{
+		int Rollno;
+	}
+
+
+	public static void main(String[] args) {
+
+				
+		// Keyword as variable		
+		//Syntax error on token "double", invalid VariableDeclaratorId
+				int double=10;
+        // Keyword as function
+		//Syntax error on token "float", invalid AnnotationName
+				void float() {
+			    System.out.println("hello");
+		}
+				//Syntax error on token "int", delete this token
+				Student int=new Student;
+				
+		}
+	}
+
+
+	
